14ce1f24a099e28918e0c75fb057043babefa209
shruti <shrutikanaik5023@gmail.com>
2022-11-14 15:47:48 +0530

    function changed

--- script.js
+++ script.js
@@ -24,157 +24,121 @@ Array.prototype.forEach.call(buttons, function(button) {
             button.textContent != "rad" &&
             button.textContent != "∘") {
             display.value += button.textContent;
-        } else if (button.textContent === "=") {
-            equals();
-        } else if (button.textContent === "C") {
-            clear();
-        } else if (button.textContent === "x") {
-            multiply();
-        } else if (button.textContent === "÷") {
-            divide();
-        } else if (button.textContent === "±") {
-            plusMinus();
-        } else if (button.textContent === "<=") {
-            backspace();
-        } else if (button.textContent === "%") {
-            percent();
-        } else if (button.textContent === "π") {
-            pi();
-        } else if (button.textContent === "x ²") {
-            square();
-        } else if (button.textContent === "√") {
-            squareRoot();
-        } else if (button.textContent === "sin") {
-            sin();
-        } else if (button.textContent === "cos") {
-            cos();
-        } else if (button.textContent === "tan") {
-            tan();
-        } else if (button.textContent === "log") {
-            log();
-        } else if (button.textContent === "ln") {
-            ln();
-        } else if (button.textContent === "x^") {
-            exponent();
-        } else if (button.textContent === "x !") {
-            factorial();
-        } else if (button.textContent === "e") {
-            exp();
-        } else if (button.textContent === "rad") {
-            radians();
-        } else if (button.textContent === "∘") {
-            degrees();
+        } else {
+            switch (buttons) {
+                case '=':
+                    if ((display.value).indexOf("^") > -1) {
+                        var base = (display.value).slice(0, (display.value).indexOf("^"));
+                        var exponent = (display.value).slice((display.value).indexOf("^") + 1);
+                        display.value = eval("Math.pow(" + base + "," + exponent + ")");
+                    } else {
+                        display.value = eval(display.value)
+                        checkLength()
+                        syntaxError()
+                    }
+                    break;
+
+                case 'C':
+                    display.value = "";
+                    break;
+
+                case 'x':
+                    display.value += "*";
+                    break;
+
+                case '÷':
+                    display.value += "/";
+                    break;
+
+                case '+':
+                    display.value += "+";
+                    break;
+
+                case '-':
+                    display.value += "-";
+                    break;
+
+                case '<=':
+                    display.value = display.value.substring(0, display.value.length - 1);
+                    break;
+
+                case '%':
+                    display.value = display.value / 100;
+                    break;
+
+                case 'π':
+                    display.value = (display.value * Math.PI);
+                    break;
+
+                case 'x ²':
+                    display.value = eval(display.value * display.value);
+                    break;
+
+                case '√':
+                    display.value = Math.sqrt(display.value);
+                    break;
+
+                case 'sin':
+                    display.value = Math.sin(display.value);
+                    break;
+
+                case 'cos':
+                    display.value = Math.cos(display.value);
+                    break;
+
+                case 'tan':
+                    display.value = Math.tan(display.value);
+                    break;
+
+                case 'log':
+                    display.value = Math.log10(display.value);
+                    break;
+
+                case 'ln':
+                    display.value = Math.log(display.value);
+                    break;
+
+                case 'x^':
+                    display.value += "^";
+                    break;
+
+                case 'x !':
+                    var number = 1;
+                    if (display.value === 0) {
+                        display.value = "1";
+                    } else if (display.value < 0) {
+                        display.value = "undefined";
+                    } else {
+                        var number = 1;
+                        for (var i = display.value; i > 0; i--) {
+                            number *= i;
+                        }
+                        display.value = number;
+                    }
+                    break;
+
+                case 'e':
+                    display.value = Math.exp(display.value);
+                    break;
+
+                case 'rad':
+                    display.value = display.value * (Math.PI / 180);
+                    break;
+
+                case '∘':
+                    display.value = display.value * (180 / Math.PI);
+                    break
+
+                case 'x<sup>3 ':
+                    display.value = eval(display.value * display.value * display.value);
+                    break;
+
+                default:
+                    if (eval(display.value) == SyntaxError || eval(display.value) == ReferenceError || eval(display.value) == TypeError) {
+                        display.value == "Syntax Error";
+                    }
+                    break;
+            }
         }
     });
 });
-
-
-function syntaxError() {
-    if (eval(display.value) == SyntaxError || eval(display.value) == ReferenceError || eval(display.value) == TypeError) {
-        display.value == "Syntax Error";
-    }
-}
-
-
-function equals() {
-    if ((display.value).indexOf("^") > -1) {
-        var base = (display.value).slice(0, (display.value).indexOf("^"));
-        var exponent = (display.value).slice((display.value).indexOf("^") + 1);
-        display.value = eval("Math.pow(" + base + "," + exponent + ")");
-    } else {
-        display.value = eval(display.value)
-        checkLength()
-        syntaxError()
-    }
-}
-
-function clear() {
-    display.value = "";
-}
-
-function backspace() {
-    display.value = display.value.substring(0, display.value.length - 1);
-}
-
-function multiply() {
-    display.value += "*";
-}
-
-function divide() {
-    display.value += "/";
-}
-
-function plusMinus() {
-    if (display.value.charAt(0) === "-") {
-        display.value = display.value.slice(1);
-    } else {
-        display.value = "-" + display.value;
-    }
-}
-
-function factorial() {
-    var number = 1;
-    if (display.value === 0) {
-        display.value = "1";
-    } else if (display.value < 0) {
-        display.value = "undefined";
-    } else {
-        var number = 1;
-        for (var i = display.value; i > 0; i--) {
-            number *= i;
-        }
-        display.value = number;
-    }
-}
-
-function pi() {
-    display.value = (display.value * Math.PI);
-}
-
-function square() {
-    display.value = eval(display.value * display.value);
-}
-
-function squareRoot() {
-    display.value = Math.sqrt(display.value);
-}
-
-function percent() {
-    display.value = display.value / 100;
-}
-
-function sin() {
-    display.value = Math.sin(display.value);
-}
-
-function cos() {
-    display.value = Math.cos(display.value);
-}
-
-function tan() {
-    display.value = Math.tan(display.value);
-}
-
-function log() {
-    display.value = Math.log10(display.value);
-}
-
-function ln() {
-    display.value = Math.log(display.value);
-}
-
-function exponent() {
-    display.value += "^";
-}
-
-function exp() {
-    display.value = Math.exp(display.value);
-}
-
-function radians() {
-    display.value = display.value * (Math.PI / 180);
-}
-
-function degrees() {
-    display.value = display.value * (180 / Math.PI);
-}
